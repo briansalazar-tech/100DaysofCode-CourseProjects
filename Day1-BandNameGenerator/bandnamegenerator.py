b0b3c18dc48a695efab0657bffc61f5ec16ac976
Brian Salazar <briansalazar5@gmail.com>
2025-01-31 10:41:34 -0800

    Finish Day 1 project

---
+++ Day1-BandNameGenerator/bandnamegenerator.py
@@ -0,0 +1,5 @@
+print("Whelcome to the Band Name Generator")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+
+print(f"Your band name could be {city} {pet}")
