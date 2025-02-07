849bdfe5aeea4b7b19d10b4f6a76443dc61e9538
Brian Salazar <briansalazar5@gmail.com>
2025-02-07 11:28:06 -0800

    combine encode and decode functionality

--- Day8-CaesarCipher/caesarcipher.py
+++ Day8-CaesarCipher/caesarcipher.py
@@ -5,25 +5,23 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-text = "khoor" # hello
-shift = 29
+def caesar(direction, original_text, shift_amount):
+    
+    output_text = ""
 
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
+    if direction == "decode":
+        shift_amount *= -1
+
     for letter in original_text:
         shifted_index = alphabet.index(letter) + shift_amount
         # If shifted amount is greater than the last index of the alphabet, then index wraps around to begining
         if shifted_index > 25:
             shifted_index = shifted_index % len(alphabet)
-        encrypted_text += alphabet[shifted_index]
-    print(encrypted_text)
+        output_text += alphabet[shifted_index]
+        
+    print(f"Your {direction}d result is: {output_text}")
 
-def decrypt(original_text, shift_amount):
-    decrypted_text = ""
-    for letter in original_text:
-        shifted_index = alphabet.index(letter) - shift_amount
-        # If shifted amount is greater than the last index of the alphabet, then index wraps around to begining
-        if shifted_index > 25:
-            shifted_index = shifted_index % len(alphabet)
-        decrypted_text += alphabet[shifted_index]
-    print(decrypted_text)
+    if direction != "encode" and direction != "decode":
+        print("Invalid input. Type encode or decode.")
+
+caesar(direction, text, shift)
