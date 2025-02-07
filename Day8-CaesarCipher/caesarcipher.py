86a581eb43a35f338ded12ee9f0453cafefe6b57
Brian Salazar <briansalazar5@gmail.com>
2025-02-07 13:51:50 -0800

    Add ascii art, address special characters, address invalid entries, and add replayability

--- Day8-CaesarCipher/caesarcipher.py
+++ Day8-CaesarCipher/caesarcipher.py
@@ -1,8 +1,5 @@
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 
 def caesar(direction, original_text, shift_amount):
@@ -13,15 +10,35 @@ def caesar(direction, original_text, shift_amount):
         shift_amount *= -1
 
     for letter in original_text:
-        shifted_index = alphabet.index(letter) + shift_amount
-        # If shifted amount is greater than the last index of the alphabet, then index wraps around to begining
-        if shifted_index > 25:
-            shifted_index = shifted_index % len(alphabet)
-        output_text += alphabet[shifted_index]
+        if letter not in alphabet:
+            print(letter)
+            output_text += letter
+        else:
+            shifted_index = alphabet.index(letter) + shift_amount
+            # If shifted amount is greater than the last index of the alphabet, then index wraps around to begining
+            if shifted_index > 25:
+                shifted_index = shifted_index % len(alphabet)
+            output_text += alphabet[shifted_index]
         
-    print(f"Your {direction}d result is: {output_text}")
+    print(f"Your {direction}d result is: {output_text}") 
 
+while True:
+    print(logo)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    # ensure that encode or decode is entered before proceeding
     if direction != "encode" and direction != "decode":
-        print("Invalid input. Type encode or decode.")
+        print(f"{direction} is not a valid input. Type encode or decode.")
+        continue
+    text = input("Type your message:\n").lower()
+    # Ensure that a valid number is entered
+    try:
+        shift = int(input("Type the shift number:\n"))
+    except:
+        print(f"Invalid input. Please enter a number.")
+        continue
+    
+    caesar(direction, text, shift)
 
-caesar(direction, text, shift)
+    prompt_again = input("Type 'yes' of you want to enter another another message. Otherwise type 'no' to exit.\n").lower()
+    if prompt_again[0] != "y":
+        break
