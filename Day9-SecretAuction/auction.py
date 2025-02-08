d85b942d92880d9e7cfc166dfe0e18dc0cbd6c41
Brian Salazar <briansalazar5@gmail.com>
2025-02-07 16:00:08 -0800

    add art.py and auction.py to Silent Auction project folder

---
+++ Day9-SecretAuction/auction.py
@@ -0,0 +1,10 @@
+from art import logo
+
+print(logo)
+print("Welcome to the secret auction program.")
+# Logic
+# Ask for name - Dictionary key
+# Ask for bid - Dictionary value
+# Ask if other bidder?
+# If yes, clear screen and repeat process above
+# After all bids submitted, check who has submitted the highest bid and declair winner
