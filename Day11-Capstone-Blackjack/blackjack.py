d318ba3adf6d19a092b2b548361f57c25d40e940
Brian Salazar <briansalazar5@gmail.com>
2025-02-10 16:37:31 -0800

    Add starting code and artwork python files along with readme containing house rules and project flow chart

---
+++ Day11-Capstone-Blackjack/blackjack.py
@@ -0,0 +1,45 @@
+import os
+from art import logo
+from random import choice
+
+CARDS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+user_hand = []
+dealer_hand = []
+
+
+def clear_screen():
+    pass
+
+
+def deal_card():
+    pass
+
+
+# Start game (or quit)
+
+# Deal cards
+
+# Display users cards
+
+# Display computers first card
+
+# User Turn
+# Draw again?
+# If yes, deal card
+# Over 21? Bust
+
+# Dealer Turn
+# If no, and not over 21, dealers turn
+
+# Under 17? Deal card
+
+# Over 17, proceed to compare totals
+
+# Compare totals
+# If dealer > 21, user wins
+
+# If user > dealer, user wins
+
+# If dealer > user, dealer wins
+
+# If dealer == user, draw 
