98f49da44d4ac2b6f282e867845aabc003228504
Brian Salazar <briansalazar5@gmail.com>
2025-02-12 09:09:26 -0800

    Add dealer turn functionality

--- Day11-Capstone-Blackjack/blackjack.py
+++ Day11-Capstone-Blackjack/blackjack.py
@@ -41,55 +41,46 @@ print(f"Your hand: {user_hand}, current score: {user_score}")
 print(f"Dealer's first card is: {dealer_hand[0]}")
 
 # Users turn
-user_turn = True
-while user_turn:
-    # user_hand = [12, 2, 10] ## Test hand
-    user_hand.append(deal_card())
-    user_score = sum(user_hand)
-    draw_card = input("\nDo you want to draw another card? Type 'y' or 'n': ")
-    # Check if user has 11 in hand and over 21
-    if 11 in user_hand and user_score > 21:
-        user_hand.remove(11)
-        user_hand.append(1)
-        user_score = sum(user_hand)
-        print(user_hand)
-        print(user_score)
-    print(f"\nYour hand: {user_hand}, current score: {user_score}")
-    print(f"Dealer's first card is: {dealer_hand[0]}")
-    # Check if user busts
-    if user_score > 21:
-        print(f"\nYour final hand: {user_hand}, final score: {user_score}")
-        print("You went over 21, you lose!")
-        user_turn = False
-        # Do not proceed to dealer turn if busts
-    # End user turn
-    if draw_card == "n":
-        print("Dealer's Turn")
-        user_turn = False
+# user_turn = True
+# while user_turn:
+#     # user_hand = [12, 2, 10] ## Test hand
+#     user_hand.append(deal_card())
+#     user_score = sum(user_hand)
+#     draw_card = input("\nDo you want to draw another card? Type 'y' or 'n': ")
+#     # Check if user has 11 in hand and over 21
+#     if 11 in user_hand and user_score > 21:
+#         user_hand.remove(11)
+#         user_hand.append(1)
+#         user_score = sum(user_hand)
+#         print(user_hand)
+#         print(user_score)
+#     print(f"\nYour hand: {user_hand}, current score: {user_score}")
+#     print(f"Dealer's first card is: {dealer_hand[0]}")
+#     # Check if user busts
+#     if user_score > 21:
+#         print(f"\nYour final hand: {user_hand}, final score: {user_score}")
+#         print("You went over 21, you lose!")
+#         user_turn = False
+#         # Do not proceed to dealer turn if busts
+#     # End user turn
+#     if draw_card == "n":
+#         print("Dealer's Turn")
+#         user_turn = False
         
 # Dealers turn
 dealer_turn = True
-# Display users cards
+while dealer_turn:
+    print(dealer_score)
+    while dealer_score < 17:
+        dealer_hand.append(deal_card())
+        dealer_score = sum(dealer_hand)
+        if 11 in dealer_hand and dealer_score > 21:
+            dealer_hand.remove(11)
+            dealer_hand.append(1)
+            dealer_score = sum(dealer_hand)
+    if dealer_score >= 17:
+        print(dealer_hand)
+        print(dealer_score)
+        dealer_turn = False
 
-# Display computers first card
-
-# User Turn
-# Draw again?
-# If yes, deal card
-# Over 21? Bust
-
-# Dealer Turn
-# If no, and not over 21, dealers turn
-
-# Under 17? Deal card
-
-# Over 17, proceed to compare totals
-
-# Compare totals
-# If dealer > 21, user wins
-
-# If user > dealer, user wins
-
-# If dealer > user, dealer wins
-
-# If dealer == user, draw 
+# Compare scores and determine winner
