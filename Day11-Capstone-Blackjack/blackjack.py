292107d0b68fc1c3bda9a0280feb903481a9518d
Brian Salazar <briansalazar5@gmail.com>
2025-02-12 11:23:17 -0800

    Put compoents together, test gameplay and add replayability

--- Day11-Capstone-Blackjack/blackjack.py
+++ Day11-Capstone-Blackjack/blackjack.py
@@ -20,67 +20,103 @@ def deal_card():
 
 
 # Start game (or quit)
+play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 
-# play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-# while play_game == "y":
-#     print(logo)
-#     print("Let's play Blackjack!")
-#     play_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-#     clear_screen()
-# print("Goodbye!")
-
-
-# Deal cards to user and dealer
-for card in range(2):
-    user_hand.append(deal_card())
-    dealer_hand.append(deal_card())
-user_score = sum(user_hand)
-dealer_score = sum(dealer_hand)
-
-print(f"Your hand: {user_hand}, current score: {user_score}")
-print(f"Dealer's first card is: {dealer_hand[0]}")
-
-# Users turn
-# user_turn = True
-# while user_turn:
-#     # user_hand = [12, 2, 10] ## Test hand
-#     user_hand.append(deal_card())
-#     user_score = sum(user_hand)
-#     draw_card = input("\nDo you want to draw another card? Type 'y' or 'n': ")
-#     # Check if user has 11 in hand and over 21
-#     if 11 in user_hand and user_score > 21:
-#         user_hand.remove(11)
-#         user_hand.append(1)
-#         user_score = sum(user_hand)
-#         print(user_hand)
-#         print(user_score)
-#     print(f"\nYour hand: {user_hand}, current score: {user_score}")
-#     print(f"Dealer's first card is: {dealer_hand[0]}")
-#     # Check if user busts
-#     if user_score > 21:
-#         print(f"\nYour final hand: {user_hand}, final score: {user_score}")
-#         print("You went over 21, you lose!")
-#         user_turn = False
-#         # Do not proceed to dealer turn if busts
-#     # End user turn
-#     if draw_card == "n":
-#         print("Dealer's Turn")
-#         user_turn = False
-        
-# Dealers turn
-dealer_turn = True
-while dealer_turn:
-    print(dealer_score)
-    while dealer_score < 17:
+# Initialize game
+if play_game == "y":
+    print(logo)
+    print("Let's play Blackjack!")
+    
+    # Deal cards to user and dealer
+    for card in range(2):
+        user_hand.append(deal_card())
         dealer_hand.append(deal_card())
-        dealer_score = sum(dealer_hand)
-        if 11 in dealer_hand and dealer_score > 21:
-            dealer_hand.remove(11)
-            dealer_hand.append(1)
-            dealer_score = sum(dealer_hand)
-    if dealer_score >= 17:
-        print(dealer_hand)
-        print(dealer_score)
-        dealer_turn = False
+    user_score = sum(user_hand)
+    dealer_score = sum(dealer_hand)
 
-# Compare scores and determine winner
+    print(f"Your hand: {user_hand}, current score: {user_score}")
+    print(f"Dealer's first card is: {dealer_hand[0]}")
+
+elif play_game == "n":
+    clear_screen()
+    print("Goodbye!")
+
+game_on = True
+while game_on and play_game == "y":
+    # Users turn
+    user_turn = True
+    while user_turn and game_on:
+        
+        draw_card = input("\nDo you want to draw another card? Type 'y' or 'n': ")
+        if draw_card == "y":
+            user_hand.append(deal_card())
+            user_score = sum(user_hand)
+        # Check if user has 11 in hand and over 21
+        if 11 in user_hand and user_score > 21:
+            user_hand.remove(11)
+            user_hand.append(1)
+            user_score = sum(user_hand)
+            print(user_hand)
+            print(user_score)
+        print(f"\nYour hand: {user_hand}, current score: {user_score}")
+        print(f"Dealer's first card is: {dealer_hand[0]}")
+        # Check if user busts
+        if user_score > 21:
+            print(f"\nYour final hand: {user_hand}, final score: {user_score}")
+            print("\tYou went over 21, you lose! 🥲")
+            user_turn = False
+            game_on = False
+        # End user turn
+        if draw_card == "n":
+            print("Dealer's Turn...")
+            user_turn = False
+            
+    # Dealers turn
+    dealer_turn = True
+    while dealer_turn and game_on:
+        while dealer_score < 17:
+            dealer_hand.append(deal_card())
+            dealer_score = sum(dealer_hand)
+            if 11 in dealer_hand and dealer_score > 21:
+                dealer_hand.remove(11)
+                dealer_hand.append(1)
+                dealer_score = sum(dealer_hand)
+        if dealer_score >= 17:
+            print(f"\nYour final hand: {user_hand}, final score: {user_score}")
+            print(f"Dealer's final hand: {dealer_hand}, final score: {dealer_score}")
+            dealer_turn = False
+    
+    # Compare scores and determine winner
+    if game_on:
+        if dealer_score > 21:
+            print("\tDealer busts! You win! 🎉")
+        elif user_score > dealer_score:
+            print("\tYou win! 🎉")
+        elif user_score < dealer_score:
+            print("\tYou lose! 🥲")
+        elif user_score == dealer_score:
+            print("\tIt is a draw! 🙅‍♂️")
+    play_again = input("\nDo you want to play a game of Blackjack? Type 'y' or 'n': ")
+    #Reset game if play again is yes
+    if play_again == "y":
+        game_on = True
+        user_turn = True
+        dealer_turn = True
+        user_hand = []
+        dealer_hand = []
+        dealer_score = 0
+        user_score = 0
+        clear_screen()
+        print(logo)
+        for card in range(2):
+            user_hand.append(deal_card())
+            dealer_hand.append(deal_card())
+        user_score = sum(user_hand)
+        dealer_score = sum(dealer_hand)
+        print(f"Your hand: {user_hand}, current score: {user_score}")
+        print(f"Dealer's first card is: {dealer_hand[0]}")
+
+    else:
+        clear_screen()
+        print("Thanks for playing!")
+        game_on = False
