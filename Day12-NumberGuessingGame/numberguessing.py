4214cb9e69357ec6d2ae3cb9c4960ee512ac6929
Brian Salazar <briansalazar5@gmail.com>
2025-02-14 09:34:17 -0800

    Add starting files for Day 12

---
+++ Day12-NumberGuessingGame/numberguessing.py
@@ -0,0 +1,6 @@
+# Todos:
+# Generate a random number
+# Chose dificulty - hard (5) or easy (10)
+# Ask user to make a guess
+# Check users guess. Inform if to high, to low or correct
+# If incorrect, minus 1 life. Guess again, else correct number guessed.
