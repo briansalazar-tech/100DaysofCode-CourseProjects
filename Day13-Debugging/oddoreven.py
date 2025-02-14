664427d244234e498d64101ac87bc245b0297a22
Brian Salazar <briansalazar5@gmail.com>
2025-02-14 15:25:30 -0800

    Add day 13 starting files

---
+++ Day13-Debugging/oddoreven.py
@@ -0,0 +1,9 @@
+# ORIGINAL CODE # 
+def odd_or_even(number):
+    if number % 2 = 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+    
+
+# DEBUGGED CODE #
