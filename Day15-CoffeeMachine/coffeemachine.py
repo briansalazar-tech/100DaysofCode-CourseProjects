4c004ab8e9347300f4e35c7db61ada039bca1f01
Brian Salazar <briansalazar5@gmail.com>
2025-02-19 14:00:39 -0800

    Create report function and add basic prompts to the coffee machine

--- Day15-CoffeeMachine/coffeemachine.py
+++ Day15-CoffeeMachine/coffeemachine.py
@@ -37,3 +37,29 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+
+
+
+def report():
+    """Prints a report of current resources."""
+
+    print("Current resources: ")
+    for resource in resources:
+        print(f"\t{resource}: {resources[resource]}")
+    print(f"\tMoney : ${money}")
+
+money = 0
+machine_on = True
+
+while machine_on:
+    # Prompt user for drink
+    drink_to_make = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    print(drink_to_make)
+    # Print report
+    if drink_to_make == "report":
+        report()
+    # Turn machine off
+    elif drink_to_make == "off":
+        print("Goodbye")
+        machine_on = False
