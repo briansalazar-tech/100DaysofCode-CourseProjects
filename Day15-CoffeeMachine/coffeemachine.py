f0dac26746745407a9074ea47c048e7a50fe8c25
Brian Salazar <briansalazar5@gmail.com>
2025-02-19 16:38:11 -0800

    Add art, move menu to it's own file and clean up code on coffeemachine.py

--- Day15-CoffeeMachine/coffeemachine.py
+++ Day15-CoffeeMachine/coffeemachine.py
@@ -1,36 +1,8 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    },
-    "overkill": {
-        "ingredients": {
-            "water": 301,
-            "milk": 201,
-            "coffee": 101,
-        },
-        "cost": 3.0,
-    }
-}
+import os
+from art import logo
+from menu import menu
+
+MENU = menu
 
 resources = {
     "water": 300,
@@ -41,16 +13,14 @@ resources = {
 
 def report():
     """Prints a report of current resources."""
-
     print("Current resources: ")
     for resource in resources:
-        print(f"\t{resource}: {resources[resource]}")
+        print(f"\t{resource.title()}: {resources[resource]}")
     print(f"\tMoney : ${money}")
 
 
 def check_resources(drink_name):
     """Checks if the coffee machine has sufficient resources for the drink that is passed through."""
-
     make_drink = True
     drink = MENU[drink_name]["ingredients"]
     print(f"{drink_name.title()} selected...")
@@ -65,19 +35,17 @@ def check_resources(drink_name):
     elif make_drink == False:
         print("Please try a different drink")
         return False
-    
+
+
 def subtract_resources(drink_name):
     """Update the quantity of resources in the coffee machine."""
-
     drink = MENU[drink_name]["ingredients"]
     for ingredient in drink:
         resources[ingredient] -= MENU[drink_name]["ingredients"][ingredient]
 
 
-
 def add_resources():
     """Add additional resources to the coffee machine."""
-
     resources["water"] += 300
     resources["milk"] += 200
     resources["coffee"] += 100
@@ -86,7 +54,6 @@ def add_resources():
 
 def process_coins():
     """Asks user to enter money into the machine. Total value of coins is returned."""
-    
     try:
         quarters = float(input("Please enter quarters ($0.25): ")) * 0.25
         dimes = float(input("Please enter dimes ($0.10): ")) * 0.10
@@ -96,71 +63,55 @@ def process_coins():
         return total
     
     except:
-        print("Invalid entry. Please enter a valid quantity")
-    
+        return
+
+
 money = 0
 machine_on = True
 
 while machine_on:
-    # TODO Prompt user for drink
+    print(logo)
     drink_to_make = input("What would you like? (espresso/latte/cappuccino): ").lower()
-    # print(drink_to_make)
-    # Pass a drink
+    
+    # User enters a drink
     if drink_to_make == "espresso"or drink_to_make == "latte" or drink_to_make == "cappuccino" or drink_to_make == "overkill":
-        # TODO Check resources sufficient
+        
+        # Check if resources sufficient
         if check_resources(drink_to_make):
             print(f"A {drink_to_make} will cost ${MENU[drink_to_make]["cost"]}")
             try: 
                 user_coins = process_coins()
-                print(user_coins)
-                # TODO Process coins & Check transaction success
+                # Process coins & Check transaction success
                 # Sufficient money entered
-                if user_coins >= MENU["cappuccino"]["cost"]:
+                if user_coins >= MENU[drink_to_make]["cost"]:
                     # User paid extra
-                    if user_coins > MENU["cappuccino"]["cost"]:
-                        change = user_coins - MENU["cappuccino"]["cost"]
+                    if user_coins > MENU[drink_to_make]["cost"]:
+                        change = user_coins - MENU[drink_to_make]["cost"]
                         rounded_change = "{:.2f}".format(change)
-                        money += MENU["cappuccino"]["cost"]
-                        print(f"Change due: {rounded_change}")
-
-                        # Continue
-                        print("Sufficient")
-                        # TODO Make Coffee
-                        subtract_resources(drink_to_make)
-                        print(f"Enjoy your {drink_to_make}! ☕")
+                        
+                        print(f"Change due: ${rounded_change}")
+                    # Transaction successful. Adjust resources
+                    subtract_resources(drink_to_make)
+                    money += MENU[drink_to_make]["cost"]
+                    print(f"Enjoy your {drink_to_make}! ☕")
 
                 # Insufficient money
-                elif user_coins < MENU["cappuccino"]["cost"]:
-                    print(f"Insufficient money entered. {drink_to_make.title()} costs ${MENU[drink_to_make]["cost"]}")
-                
-                print(f"Machine money: {money}")
+                elif user_coins < MENU[drink_to_make]["cost"]:
+                    print(f"Insufficient money entered. {drink_to_make.title()} costs ${MENU[drink_to_make]["cost"]}, you entered ${user_coins}")
             
             # Integer not entered
             except:
-                continue
+                print("Invalid entry. Please enter a valid quantity")
     
-    # TODO Print report
     elif drink_to_make == "report":
         report()
 
     elif drink_to_make == "add":
         add_resources()
-    
-    # TODO Turn machine off
-    elif drink_to_make == "off":
+
+    elif drink_to_make == "off" or drink_to_make == "exit":
         print("Goodbye")
         machine_on = False
     
     else:
         print("That is not a valid choice. Try again.")
-
-
-
-# TODO Make Coffee
-# print(resources)
-# subtract_resources("cappuccino")
-# print("Drink has been made")
-# print(resources)
-# print("Adding resources")
-# add_resources()
-# print(resources)
