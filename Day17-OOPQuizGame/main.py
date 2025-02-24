1cc6eda026d2f2d01bd8101e3220f5aa1a6f52b0
Brian Salazar <briansalazar5@gmail.com>
2025-02-24 11:44:26 -0800

    Add score checking functionality

--- Day17-OOPQuizGame/main.py
+++ Day17-OOPQuizGame/main.py
@@ -13,3 +13,6 @@ for item in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You have completed the quiz!")
+print(f"Your final score was: {quiz.score} / {len(question_bank)}")
