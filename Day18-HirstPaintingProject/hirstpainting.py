75f443da21dfceed6963fb2c20be17e611d83665
Brian Salazar <briansalazar5@gmail.com>
2025-02-26 20:22:06 -0800

    Use colorgram.extract to generate color list. Comment out colorgram section once list of colors obained since it is not necessary to rerun

---
+++ Day18-HirstPaintingProject/hirstpainting.py
@@ -0,0 +1,13 @@
+# import colorgram
+
+# rgb_colors = []
+# colors = colorgram.extract("image.jpg", 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+# print(rgb_colors)
+
+color_list = [(177, 2, 82), (181, 154, 117), (187, 162, 16), (39, 139, 51), (214, 24, 241), (82, 160, 202), (46, 86, 165), (252, 229, 22), (10, 1, 69), (124, 65, 31), (248, 1, 0), (53, 153, 128), (254, 4, 9), (237, 159, 205), (254, 5, 3), (238, 59, 54), (105, 49, 16), (105, 119, 169), (42, 57, 112), (247, 160, 153), (164, 209, 189)]
