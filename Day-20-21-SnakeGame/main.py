b95b881c6c0d748189d676108b5b6a3fe7f2d59f
Brian Salazar <briansalazar5@gmail.com>
2025-02-28 15:36:48 -0800

    Add starting files and code for snake game project

---
+++ Day-20-21-SnakeGame/main.py
@@ -0,0 +1,18 @@
+# TODO: Create a snake body
+# TODO: Move the snake
+# TODO: Create snake food
+# TODO: Detect collision with food
+# TODO: Create a scoreboard
+# TODO: Detect collision with wall
+# TODO: Detect collision with tail
+
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Python Snake Game")
+
+
+
+screen.exitonclick()
