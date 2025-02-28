6e60c136c1c86ed0d1ad69a84557db34e6762c69
Brian Salazar <briansalazar5@gmail.com>
2025-02-28 14:03:52 -0800

    Add turtle race capabilities and declare winner

--- Day19-TurtleRaces/turtleraces.py
+++ Day19-TurtleRaces/turtleraces.py
@@ -1,15 +1,18 @@
+import random
 from turtle import Turtle, Screen
 
 # Screen setup & prompt
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
 
+# Starting variables
 starting_y = -150
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 turtles = []
+is_race_on = False
 
+# Generate turtles
 for color in colors:
     turtle = Turtle(shape="turtle")
     turtle.color(color)
@@ -18,5 +21,20 @@ for color in colors:
     turtles.append(turtle)
     starting_y += 50
 
+if user_bet:
+    is_race_on = True
+
+# Turtle race. Race ends when a turtle crosses the xcor specified.
+while is_race_on:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You win! The {winning_color} turtle is the winner! 🎉 ")
+            else:
+                print(f"You lose. The {winning_color} turtle is the winner!")
+        random_distance = random.randint(0,10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
