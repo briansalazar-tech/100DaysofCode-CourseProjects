7bfd94744e2319583a29061f7ab9580509b2312b
Brian Salazar <briansalazar5@gmail.com>
2025-03-04 10:50:38 -0800

    Add functionality to move the snake's body

--- Day-20-21-SnakeGame/main.py
+++ Day-20-21-SnakeGame/main.py
@@ -6,22 +6,34 @@
 # TODO: Detect collision with tail
 
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Python Snake Game")
 
+
 # TODO: Create a snake body
 starting_x = 0
 snake_body = []
+
 for segment in range(3):
     snake = Turtle(shape="square")
-    snake.penup()
     snake.color("white")
+    snake.penup()
     snake.goto(x=starting_x,y=0)
     starting_x -= 20
-    snake_body.append(segment)
+    snake_body.append(snake)
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
+    for segment in range(len(snake_body) -1, 0, -1):
+        new_x = snake_body[segment -1].xcor()
+        new_y = snake_body[segment -1].ycor()
+        snake_body[segment].goto(new_x, new_y)
+    snake_body[0].forward(20)
 screen.exitonclick()
