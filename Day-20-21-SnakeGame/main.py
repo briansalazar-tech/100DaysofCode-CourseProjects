b9150545aa3764ddb5f61dcb196634e7a6dc30c2
Brian Salazar <briansalazar5@gmail.com>
2025-03-05 09:44:43 -0800

    Add food.py and ability for food to spawn. Add feature for food to respawn when collision with snake is detected

--- Day-20-21-SnakeGame/main.py
+++ Day-20-21-SnakeGame/main.py
@@ -5,7 +5,8 @@
 # TODO: Detect collision with tail
 import time
 from snake import Snake
-from turtle import Screen, Turtle
+from food import Food
+from turtle import Screen
 
 
 screen = Screen()
@@ -13,7 +14,9 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Python Snake Game")
 
+food = Food()
 snake = Snake()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -26,5 +29,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect colision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
