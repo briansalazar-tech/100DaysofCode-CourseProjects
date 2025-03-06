18488cdde969f18cefc347cc241340885272bf5f
Brian Salazar <briansalazar5@gmail.com>
2025-03-05 16:03:38 -0800

    Add starting files to Day 22 pong game project

---
+++ Day22-PongGame/main.py
@@ -0,0 +1,8 @@
+# TODO Create Screen height-600 width-800
+# TODO Create and move a paddle
+# TODO Create another padle
+# TODO Create the ball and make it move
+# TODO Detect collision with wall and bounce
+# TODO Detect collision with paddle
+# TODO Detect when paddle misses
+# TODO Keep Score
