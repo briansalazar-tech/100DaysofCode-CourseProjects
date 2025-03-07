4cb983de29ae2932882454e7d91a8743d118d721
Brian Salazar <briansalazar5@gmail.com>
2025-03-07 14:48:59 -0800

    Add player turtle functionality

--- Day23-Capstone-TurtleCrossing/player.py
+++ Day23-Capstone-TurtleCrossing/player.py
@@ -1,7 +1,26 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def move(self):
+        """Moves the turtle forward by the specified MOVE_DISTANCE"""
+        self.forward(MOVE_DISTANCE)
+
+    def level_up(self):
+        """Returns the turtle back to the STARTING_POSITION withen FINISH_LINE_Y is crossed"""
+        if self.ycor() == FINISH_LINE_Y:
+            print("Reset the turtle")
+            self.goto(STARTING_POSITION)
