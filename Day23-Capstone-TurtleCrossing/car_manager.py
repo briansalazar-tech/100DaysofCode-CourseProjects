c427a1913523a645b030686784b5b774ca761a71
Brian Salazar <briansalazar5@gmail.com>
2025-03-10 15:09:52 -0700

    Add Car class and CarManager class to control movement and creation of car objects. On main.py, add features to increase car speed and detect colision with player

--- Day23-Capstone-TurtleCrossing/car_manager.py
+++ Day23-Capstone-TurtleCrossing/car_manager.py
@@ -1,7 +1,44 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+MOVE_INCREMENT = 1
+STARTING_Y = [-230, -200, -150, -100, -50, 0, 50, 100, 150, 200, 250]
+STARTING_X = [300, 350, 400, 450, 500, 550, 600, 650, 700]
 
+class Car(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.color(random.choice(COLORS))
+        self.goto(random.choice(STARTING_X), random.choice(STARTING_Y))
 
 class CarManager:
-    pass
+    
+    def __init__(self):
+        """Creates 15 cars to start out the game and appends them to the all_cars list. CarManager will be used to control the movement of the cars in the all_cars list"""
+        self.all_cars = []
+        for car in range(15):
+            self.create_car()
+
+    def create_car(self):
+        """Creates a single car and appends it to the all_cars_list"""
+        new_car = Car()
+        self.all_cars.append(new_car)
+        print(len(self.all_cars))
+
+    def move_cars(self):
+        """Moves the cars in the all_cars list to the left. Once a car reaches the left side of the screen, it is respawned off screen on the right side"""
+        for car in self.all_cars:
+            if car.xcor() < -300:
+                car.goto(random.choice(STARTING_X), random.choice(STARTING_Y))
+            car.backward(STARTING_MOVE_DISTANCE)
+
+    def increase_speed(self):
+        """Increase the speed of the cars by the specieid MOVE_INCREMENT value"""
+        global STARTING_MOVE_DISTANCE
+        STARTING_MOVE_DISTANCE += MOVE_INCREMENT
--- Day23-Capstone-TurtleCrossing/main.py
+++ Day23-Capstone-TurtleCrossing/main.py
@@ -5,11 +5,13 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+screen.title("Turtle Crossing Game")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
 scoreboard = Scoreboard()
+cars = CarManager()
 
 screen.listen()
 screen.onkey(key="Up", fun=player.move)
@@ -17,13 +19,22 @@ screen.onkey(key="Up", fun=player.move)
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+
+    cars.move_cars()
+
     if player.ycor() >= 280:
         player.level_up()
         scoreboard.level_up()
+        cars.increase_speed()
+        # Add a new car to the list of cars every other time the level is increased
+        if scoreboard.level % 2 == 0:
+            cars.create_car()
 
-    if player.ycor() >= 0:
-        scoreboard.game_over()
-        game_is_on = False
+    # Game ends if the player collides with a car
+    for car in cars.all_cars:
+        if player.distance(car) < 20:
+            scoreboard.game_over()
+            game_is_on = False
 
     screen.update()
 
