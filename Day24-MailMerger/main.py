24d735825e45ba5ddb3761cb34085cee757b648e
Brian Salazar <briansalazar5@gmail.com>
2025-03-12 15:39:02 -0700

    Complete mail merger script challenge. Verify output files save successfully in correct folder

--- Day24-MailMerger/main.py
+++ Day24-MailMerger/main.py
@@ -1,8 +1,27 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
+name_list = []
+template = ""
+
+# Read invited_names.txt and add names to name_list
+with open("Day24-MailMerger/Input/Names/invited_names.txt", mode="r") as names_file:
+    names = names_file.readlines()
+    name_list = names
+
+# Strip newline character from each name
+for name in range(len(name_list)):
+    name_list[name] = name_list[name].strip("\n")
+
+# Save starting_letter to letter_text
+with open("Day24-MailMerger/Input/Letters/starting_letter.txt", mode="r") as letter_file:
+        template = letter_file.read()
+
+# Repalce placeholder and sender name in template and create new letter to send
+for name in name_list:
+    letter_text = template
+    letter_text = letter_text.replace("[name]", name)
+    letter_text = letter_text.replace("Angela", "Brian")
+    # print(letter_text)
     
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+    # Save the letters in the folder "ReadyToSend"
+    with open(f"Day24-MailMerger/Output/ReadyToSend/letter_for_{name}.txt", mode="w") as letter:
+        letter.write(letter_text)
+
