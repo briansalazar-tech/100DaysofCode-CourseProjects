2282f76816de10f526dba12667af27c018f72530
Brian Salazar <briansalazar5@gmail.com>
2025-03-14 15:01:14 -0700

    Add feature to create CSV of missing states and save results in Results folder

--- Day25-USStatesGame/main.py
+++ Day25-USStatesGame/main.py
@@ -1,11 +1,17 @@
 import turtle
 import pandas
+from datetime import datetime
 
-total_guessed = 0
-correct_states = []
+# Get current date and time to append on CSV file name
+now = datetime.now()
+formated_date = now.strftime("%b-%d-%Y_%I.%M%p")
 
 # Read data from US States CSV file
 data = pandas.read_csv("./Day25-UsStatesGame/50_states.csv")
+
+correct_guesses = 0
+correct_states = []
+all_states = data["state"].to_list()
 
 
 def check_state_in_csv(state):
@@ -16,25 +22,6 @@ def check_state_in_csv(state):
         y = int(state.y.values[0])
         return (x, y)
     return "Incorrect"
-# state = data[data.state == "Texas"]
-# print(state)
-# print(state.x.values[0])
-# print(state.y.values[0])
-# x = state["x"]
-# y = state["y"]
-
-# cor = (x, y)
-# print(cor)
-# states_dict = data.to_dict()
-
-# for states in states_dict:
-#     print(states)
-
-# check = check_state_in_csv("California")
-
-
-
-
 
 # Screen setup
 screen = turtle.Screen()
@@ -43,30 +30,36 @@ image = "./Day25-UsStatesGame/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# Pen setup
 pen = turtle.Turtle()
 pen.penup()
 pen.hideturtle()
-pen.goto(0,0)
-pen.write("San Francisco", align="center", font=("Arial", 8, "normal"))
 
-
-
-# def get_mouse_click(x,y):
-#     """Get the coordinates of the mouse click on the screen"""
-#     print(x,y)
-# turtle.onscreenclick(get_mouse_click)
-
-while True:
+while len(correct_states) < 50:
     try:
-        answer_state = screen.textinput(title=f"{total_guessed}/50 States Correct", prompt="Guess the name of a state").title()
+        answer_state = screen.textinput(title=f"{correct_guesses}/50 States Correct", prompt="Guess the name of a state").title()
         check_state = check_state_in_csv(answer_state)
+        
+        if answer_state == "Exit":
+            print("Exit typed")
+            break
+
         if check_state != "Incorrect":
             correct_states.append(answer_state)
-            total_guessed += 1
+            correct_guesses += 1
             pen.goto(check_state)
             pen.write(f"{answer_state}", align="center", font=("Arial", 8, "normal"))
+            all_states.remove(answer_state)
 
     except AttributeError:
         print("Cancel/Exit button clicked")
         break
-turtle.exitonclick()
+
+# Create CSV from states user did not guess
+states_to_learn = {
+    "State Names to Study": all_states
+}
+
+states_to_learn_df = pandas.DataFrame(states_to_learn)
+states_to_learn_df.to_csv(f"./Day25-USStatesGame/Results/states_to_learn-{formated_date}.csv")    
+print("CSV file created in Results folder")
