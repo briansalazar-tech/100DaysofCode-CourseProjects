c42af5c9a22a439a1229c0fb4005f6fba3fa911f
Brian Salazar <briansalazar5@gmail.com>
2025-03-24 14:59:06 -0700

    Add ability to save website, username, and passwords in entry field to data.txt file. Default username added to the username entry field. Tested and verified that data saved to data.txt

--- Day29-PasswordManager/main.py
+++ Day29-PasswordManager/main.py
@@ -2,6 +2,25 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    """Get the values in the website, username and password entry fields and save the values to a file. 
+    Entry fields are reset to default values after data is added."""
+
+    # Get entry box values and save them to variable for readability
+    web = website_entry.get()
+    user = username_entry.get()
+    password = password_entry.get()
+    # Write data to a text file
+    with open("./Day29-PasswordManager/data.txt", mode="a") as data:
+        data.write(f"Website: {web} | Username: {user} | Password: {password}\n")
+    
+    # Clear & reset entry fields
+    website_entry.delete(first=0, last=END)
+    username_entry.delete(first=0, last=END)
+    username_entry.insert(END, "username@example.com")
+    password_entry.delete(first=0, last=END)
+
+    print("Data written to data.txt and entry fields reset to defaults")
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Window
@@ -22,6 +41,7 @@ website_label.grid(column=0, row=1)
 # Website entry
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 # Username label
 username_label = Label(text="Email/Username:")
@@ -30,6 +50,7 @@ username_label.grid(column=0, row=2)
 # Username entry
 username_entry = Entry(width=35)
 username_entry.grid(column=1, row=2, columnspan=2)
+username_entry.insert(END, "username@example.com")
 
 # Password label
 password_label = Label(text="Password:")
@@ -44,7 +65,7 @@ generate_button = Button(text="Generate")
 generate_button.grid(column=2, row=3)
 
 # Add button
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
