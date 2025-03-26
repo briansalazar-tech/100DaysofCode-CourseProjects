687044f6374b6c71653bdfb8c0c630c432074b02
Brian Salazar <briansalazar5@gmail.com>
2025-03-26 11:24:40 -0700

    Copy Day 29 files to Day 30 folder. Files will be used for Password Manager 2.0

---
+++ Day30-PasswordManager2.0/main.py
@@ -0,0 +1,111 @@
+import pyperclip
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    """Generates a random secure password."""
+    
+    password_letters = [choice(letters) for item in range(randint(8, 10))]
+    password_numbers = [choice(numbers) for item in range(randint(2, 4))]
+    password_symbols = [choice(symbols) for item in range(randint(2, 4))]
+  
+    password_list = password_letters + password_numbers + password_symbols
+
+    # Shuffle the password_List to randomize order of characters
+    shuffle(password_list)
+    password = "".join(password_list)
+    pyperclip.copy(password)
+
+    if len(password_entry.get()) == 0:
+        password_entry.insert(END, password)
+        print(password)
+    else:
+        # If entry already has value, entry is deleted prior to displaying new generated password
+        password_entry.delete(first=0, last=END)
+        password_entry.insert(END, password)
+        print(password)
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    """Get the values in the website, username and password entry fields and save the values to a file. 
+    Entry fields are reset to default values after data is added."""
+
+    # Get entry box values and save them to variable for readability
+    web = website_entry.get()
+    user = username_entry.get()
+    password = password_entry.get()
+
+    # Check and make sure that entry fields are not empty
+    if len(web) == 0 or len(password) == 0 or len(user) == 0:
+        messagebox.showwarning(title="Missing data", message="Required data is missing!\nMake sure website, username, and password fields are not empty.")
+    
+    else:
+        # Ask user if they are okay with data provided
+        save = messagebox.askokcancel(title=web, message=f"Details provided:\nWebsite: {web}\nUsername: {user}\nPassword: {password}")
+        if save:
+            # Write data to a text file
+            with open("./Day29-PasswordManager/data.txt", mode="a") as data:
+                data.write(f"Website: {web} | Username: {user} | Password: {password}\n")
+            
+            # Clear & reset entry fields
+            website_entry.delete(first=0, last=END)
+            username_entry.delete(first=0, last=END)
+            username_entry.insert(END, "username@example.com")
+            password_entry.delete(first=0, last=END)
+
+            print("Data written to data.txt and entry fields reset to defaults")
+            messagebox.showinfo(title="Success", message="Password saved to data.txt file")
+
+# ---------------------------- UI SETUP ------------------------------- #
+# Window
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+# Logo canvas
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+lock_logo = PhotoImage(file="./Day29-PasswordManager/logo.png")
+canvas.create_image(100, 100, image=lock_logo)
+canvas.grid(column=1, row=0)
+
+# Website label
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+# Website entry
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+
+# Username label
+username_label = Label(text="Email/Username:")
+username_label.grid(column=0, row=2)
+
+# Username entry
+username_entry = Entry(width=35)
+username_entry.grid(column=1, row=2, columnspan=2)
+username_entry.insert(END, "username@example.com")
+
+# Password label
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# Password entry
+password_entry = Entry(width=25)
+password_entry.grid(column=1, row=3)
+
+# Generate Password button
+generate_button = Button(text="Generate", command=generate_password)
+generate_button.grid(column=2, row=3)
+
+# Add button
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
