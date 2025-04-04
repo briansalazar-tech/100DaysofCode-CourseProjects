ee983d4a938a51d98d92f6d1b6fbdd1a07cb17ca
Brian Salazar <briansalazar5@gmail.com>
2025-04-04 12:02:34 -0700

    Clean up code on the Quiz app by removing print statements, commented out code, and adding docstrings. Add ability to inform user if answer is correct or not and disable buttons once quiz is over

--- Day34-TriviaApp/quiz_brain.py
+++ Day34-TriviaApp/quiz_brain.py
@@ -5,30 +5,32 @@ class QuizBrain:
 
     def __init__(self, q_list):
         self.question_number = 0
-        self.score = 0
         self.question_list = q_list
         self.current_question = None
 
+
     def still_has_questions(self):
+        """Checks if current question number is less than the list of questions (10)"""
         return self.question_number < len(self.question_list)
 
+
     def next_question(self):
+        """Loads the next question from the list of questions and increments question number by 1"""
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
+        print(f"Question: {self.question_number}")
         return f"Q{self.question_number}: {q_text} (True/False): "
-        #user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        # self.check_answer(user_answer)
+
 
     def check_answer(self, user_answer):
+        """Checks if the answer is correct"""
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
-            self.score += 1
-            print("You got it right!")
+
+            print("Correct")
             return True
         else:
-            print("That's wrong.")
+            print("Incorrect")
             return False
 
-        # print(f"Your current score is: {self.score}/{self.question_number}")
-        # print("\n")
--- Day34-TriviaApp/ui.py
+++ Day34-TriviaApp/ui.py
@@ -6,25 +6,25 @@ THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self, quiz_brain: QuizBrain):
+    def __init__(self, quiz_brain: QuizBrain): # Required to use QuizBrain
         self.quiz = quiz_brain
-
+        # Window setup
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
-
+        # Score label
         self.score = 0
         self.score_label = Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white")
         self.score_label.grid(row=0, column=1)
-
+        # Current Question canvas
         self.question_canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.question_canvas.create_text(150, 125, width=280, text="Question", font=("Arial", 20, "italic"))
         self.question_canvas.grid(row=1, column=0, columnspan=2, pady=50)
-
+        # True button
         self.true_image = PhotoImage(file="./Day34-TriviaApp/images/true.png")
         self.true_button = Button(image=self.true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
-
+        # False button
         self.false_image = PhotoImage(file="./Day34-TriviaApp/images/false.png")
         self.false_button = Button(image=self.false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
@@ -33,20 +33,42 @@ class QuizInterface:
 
         self.window.mainloop()
 
+
     def get_next_question(self):
-        question_text = self.quiz.next_question()
-        self.question_canvas.itemconfig(self.question_text, text=question_text)
+        """Gets the next question from quiz brain and displays it in the UI. If there are no more questions, buttons are disabled"""
+        self.question_canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            question_text = self.quiz.next_question()
+            self.question_canvas.itemconfig(self.question_text, text=question_text)
+        else:
+            self.question_canvas.itemconfig(self.question_text, text="You have reached the end of the quiz.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
 
     def true_pressed(self):
-        self.quiz.check_answer("True")
-        if self.quiz.check_answer("True"):
+        """Checks if the True button is pressed. If answer is correct, score increased by 1"""
+        answer_check = self.quiz.check_answer("True")
+        if answer_check:
             self.score += 1
             self.score_label.config(text=f"Score: {self.score}")
-        self.get_next_question()
+        self.give_feedback(answer_check)
+
 
     def false_pressed(self):
-        self.quiz.check_answer("False")
-        if self.quiz.check_answer("True"):
+        """Checks if the False button is pressed. If answer is correct, score increased by 1"""
+        answer_check = self.quiz.check_answer("False")
+        if answer_check:
             self.score += 1
             self.score_label.config(text=f"Score: {self.score}")
-        self.get_next_question()
+        self.give_feedback(answer_check)
+
+
+    def give_feedback(self, is_right):
+        """Change the background of the canvas based on if the aanswer provd by the user is correct or not"""
+        if is_right:
+            self.question_canvas.config(bg="green")
+        else:
+            self.question_canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+        
