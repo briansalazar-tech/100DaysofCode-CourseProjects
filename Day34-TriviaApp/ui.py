6d1d09e29bd8ad3bf95e770cef2381452891a3f5
Brian Salazar <briansalazar5@gmail.com>
2025-04-04 10:23:04 -0700

    Add ability for question to be displayed, buttons to take input and check user answer against correct answer

--- Day34-TriviaApp/ui.py
+++ Day34-TriviaApp/ui.py
@@ -1,28 +1,52 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
-        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score = 0
+        self.score_label = Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white")
         self.score_label.grid(row=0, column=1)
 
         self.question_canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.question_canvas.create_text(150, 125, text="Question", font=("Arial", 20, "italic"))
+        self.question_text = self.question_canvas.create_text(150, 125, width=280, text="Question", font=("Arial", 20, "italic"))
         self.question_canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.true_image = PhotoImage(file="./Day34-TriviaApp/images/true.png")
-        self.true_button = Button(image=self.true_image, highlightthickness=0, command="")
+        self.true_button = Button(image=self.true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0)
 
         self.false_image = PhotoImage(file="./Day34-TriviaApp/images/false.png")
-        self.false_button = Button(image=self.false_image, highlightthickness=0, command="")
+        self.false_button = Button(image=self.false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        question_text = self.quiz.next_question()
+        self.question_canvas.itemconfig(self.question_text, text=question_text)
+
+    def true_pressed(self):
+        self.quiz.check_answer("True")
+        if self.quiz.check_answer("True"):
+            self.score += 1
+            self.score_label.config(text=f"Score: {self.score}")
+        self.get_next_question()
+
+    def false_pressed(self):
+        self.quiz.check_answer("False")
+        if self.quiz.check_answer("True"):
+            self.score += 1
+            self.score_label.config(text=f"Score: {self.score}")
+        self.get_next_question()
