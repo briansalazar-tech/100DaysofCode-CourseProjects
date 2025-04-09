8f26e19c5929e4b31fd29a333304fd6760b60cea
Brian Salazar <briansalazar5@gmail.com>
2025-04-09 11:02:05 -0700

    Add ability to create, update and delete pixels on graph

--- Day37-HabbitTracker/main.py
+++ Day37-HabbitTracker/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,7 +8,18 @@ load_dotenv()
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 TOKEN = os.getenv("PIXELA_TOKEN")
 USERNAME = os.getenv("PIXELA_USER")
+GRAPH_ID = "graph1"
 
+# PIXEL DATA. Change values and uncomment desired activity to perform
+date = datetime(year=2025, month=4, day=3) # Enter the date you want to log data for
+formatted_date =date.strftime("%Y%m%d")
+quantity = 1.0 # Enter the quantity to log
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+## Step 1: User creation - Once account is created, lines can be commented out.
 user_parameters = {
     "token": TOKEN,
     "username": USERNAME,
@@ -15,11 +27,10 @@ user_parameters = {
     "notMinor": "yes"
 }
 
-# Step 1: User creation - Once account is created, lines can be commented out.
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_parameters)
 # print(response.text)
 
-# Step 2: Create a graph
+## Step 2: Create a graph ##
 graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
 
 graph_configuration = {
@@ -30,9 +41,32 @@ graph_configuration = {
     "color": "sora",
 }
 
-headers = {
-    "X-USER-TOKEN": TOKEN
+# response = requests.post(url=graph_endpoint, json=graph_configuration, headers=headers)
+# print(response.text)
+
+## Step 3: Log pixel data ##
+pixel_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": formatted_date, # YYYMMDD format
+    "quantity": str(quantity),
 }
 
-response = requests.post(url=graph_endpoint, json=graph_configuration, headers=headers)
+response = requests.post(url=pixel_endpoint, json=pixel_data, headers=headers)
 print(response.text)
+
+## Step 4: Update pixel data ##
+pixel_update_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{formatted_date}"
+
+update_data = {
+    "quantity": str(quantity),
+}
+
+# response = requests.put(url=pixel_update_endpoint, json=update_data, headers=headers)
+# print(response.text)
+
+## Step 5: Delete pixel data ##
+pixel_delete_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}/{formatted_date}"
+
+# response = requests.delete(url=pixel_delete_endpoint, headers=headers)
+# print(response.text)
