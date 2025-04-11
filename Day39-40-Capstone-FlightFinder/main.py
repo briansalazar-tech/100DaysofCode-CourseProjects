406e3e7d82dede4c0353106bad3aee7448733622
Brian Salazar <briansalazar5@gmail.com>
2025-04-11 09:35:52 -0700

    Add Day 39/40 Capstone project starting files

---
+++ Day39-40-Capstone-FlightFinder/main.py
@@ -0,0 +1,10 @@
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+## Requirements ##
+# Use Flight Search and SHeetpy API to populate your own copy of the Google Sheet with IATA codes for each city. Use City Code, not airport code
+# Use Flight Search API to check the cheapest flight from tomorrow to 6 months later for all cities in google sheet
+# If price is lower than lowest price in googlee sheet, send email using SMTP Lob
+# Message should include the departure airport IATA COde, destination airport IATA code, flight price and flight dates
+# TODO: Setup Google Spreedsheet
+# TODO: Setup Sheetly Project
+# TODO: Register with the Amadeus Flight Search API
+# TODO: Setup SElf Service App
