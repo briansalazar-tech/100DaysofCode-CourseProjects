41427e39aaa68d5c262531e4818f563ab2828d4d
Brian Salazar <briansalazar5@gmail.com>
2025-04-23 16:08:35 -0700

    Add Day 46 starting files

---
+++ Day46-MusicalTimeMachine/main.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+# TODO 1 Scrape Billboard Hot 100
+# TODO 2 Authentication with Spotify
+# TODO 3 Search Spotidy for Sings from Step 1
+# TODO 4 Creating and Adding to Spotify Playlist
