622034e52a5108c3298c1e732a263d39dc7f3739
Brian Salazar <briansalazar5@gmail.com>
2025-05-21 10:40:35 -0700

    Create list of posts from api endpoint

--- Day57-Capstone-BlogSitePt1/post.py
+++ Day57-Capstone-BlogSitePt1/post.py
@@ -1,2 +1,20 @@
+import requests
+response = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
 class Post:
-    pass
+
+    def __init__(self, post):
+        self.id = post["id"]
+        self.title = post["title"]
+        self.subtitle = post["subtitle"]
+        self.body = post["body"]
+
+posts_list = []
+
+for post in response:
+    new_post = Post(post)
+    posts_list.append(new_post)
+
+# for post in posts_list:
+#     if post.id == 2:
+#         print(post.title)
