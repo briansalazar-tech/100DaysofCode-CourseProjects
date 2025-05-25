dc76530b1e5cd5835f12c17305934ff0685e81d5
Brian Salazar <briansalazar5@gmail.com>
2025-05-25 12:34:00 -0700

    Move practice files to practice folder and download starting files

--- Day60-ContactForm/main.py
+++ Day60-ContactForm/main.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
 
-@app.route("/login", methods=["GET","POST"])
-def receive_data():
-    if request.method == "POST":
-        name = request.form["username"]
-        password = request.form["password"]
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
-        return f"<h1>Name: {name}, Password: {password}. Don't send a real password ;)</h1>"
-    else:
-        return "invalid request method"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
