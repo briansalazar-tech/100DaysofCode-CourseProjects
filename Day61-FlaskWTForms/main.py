f137b4438f48845bb45c7296003a4e12d57ac8ca
Brian Salazar <briansalazar5@gmail.com>
2025-05-26 12:29:00 -0700

    Complete form and block exercises

--- Day61-FlaskWTForms/main.py
+++ Day61-FlaskWTForms/main.py
@@ -1,26 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, Length
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+class LoginForm(FlaskForm):
+    email = EmailField(label="Email", validators=[DataRequired(message="Data is required"), Length(min=6, max=30), Email()])
+    password = PasswordField(label="Password", validators=[DataRequired(message="Data is required"), Length(min=8, max=30)])
+    submit = SubmitField(label="Log in")
 
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
+bootstrap = Bootstrap5(app)
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    login_form.validate_on_submit()
+    
+    if login_form.validate_on_submit():
+        username = (login_form.email.data)
+        password = (login_form.password.data)
+        if username == "admin@email.com" and password == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    
+    return render_template("login.html", form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
