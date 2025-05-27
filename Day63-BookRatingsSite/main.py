17c08ba80cd5d299826eef80568682192a23cd39
Brian Salazar <briansalazar5@gmail.com>
2025-05-27 11:03:11 -0700

    Build book website using a list to stored data

--- Day63-BookRatingsSite/main.py
+++ Day63-BookRatingsSite/main.py
@@ -1,33 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 
 all_books = []
 
-
+# Flask routes
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        book_title = request.form["bookname"]
+        book_author = request.form["bookauthor"]
+        book_rating = request.form["bookrating"]
+        
+        book_entry = {
+            "title": book_title,
+            "author": book_author,
+            "rating": book_rating
+        }
+        all_books.append(book_entry)
+        print(all_books)
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
