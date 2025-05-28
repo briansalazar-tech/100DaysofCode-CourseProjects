da3276f9b79c6653dcb386ec17266fab915ea192
Brian Salazar <briansalazar5@gmail.com>
2025-05-27 22:00:50 -0700

    Add ability to edit rating and delete entries

--- Day63-BookRatingsSite/main.py
+++ Day63-BookRatingsSite/main.py
@@ -17,9 +17,7 @@ class Base(DeclarativeBase):
     pass
 
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
-
 db = SQLAlchemy(model_class=Base)
-
 db.init_app(app)
 
 
@@ -33,10 +31,10 @@ class Book(db.Model):
 with app.app_context():
     db.create_all()
 
+
 # Flask routes
 @app.route('/')
 def home():
-
     result = db.session.execute(db.select(Book).order_by(Book.title))
     all_books = result.scalars()
 
@@ -46,7 +44,6 @@ def home():
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-
         book_title = request.form["bookname"]
         book_author = request.form["bookauthor"]
         book_rating = float(request.form["bookrating"])
@@ -61,16 +58,40 @@ def add():
             db.session.add(new_book)
             db.session.commit()
         
-        # book_entry = {
-        #     "title": book_title,
-        #     "author": book_author,
-        #     "rating": book_rating
-        # }
-        # all_books.append(book_entry)
-        print(all_books)
         return redirect(url_for("home"))
     
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    book_id = request.args.get("id")
+    # current_book = db.get_or_404(Book, book_id) Accomplishes the same as below but with less code
+    current_book = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+
+    if request.method == "POST":
+        book_rating = float(request.form["rating"])
+
+        with app.app_context():
+            book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+            book_to_update.rating = book_rating
+            db.session.commit()
+
+            return redirect(url_for("home"))
+
+    return render_template("edit.html", book=current_book)
+
+
+@app.route("/delete", methods=["GET"])
+def delete():
+    book_id = request.args.get("id")
+
+    with app.app_context():
+        book_to_delete = db.get_or_404(Book, book_id)
+        db.session.delete(book_to_delete)
+        db.session.commit()
+
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
